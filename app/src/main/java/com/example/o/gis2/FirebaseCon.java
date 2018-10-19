d7d9bda373aad3fd075cabd49ec16cf4458493aa
O <chavitak@gmail.com>
2018-10-19 15:02:21 +0900

    Location Insert 대충 완성.

--- app/src/main/java/com/example/o/gis2/FirebaseCon.java
+++ app/src/main/java/com/example/o/gis2/FirebaseCon.java
@@ -1,8 +1,10 @@
 package com.example.o.gis2;
 
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 
 public class FirebaseCon {
 
@@ -12,11 +14,20 @@ public class FirebaseCon {
     {
 
         DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference siRef = mRootRef.child("Si");
-        DatabaseReference gooRef = mRootRef.child("Goo");
+        DatabaseReference locationRef = mRootRef.child("Location");
+        String key = locationRef.push().getKey();
+        DatabaseReference uid = locationRef.child(key).child("UID");
+        DatabaseReference AddressStr = locationRef.child(key).child("Address");
+        DatabaseReference siRef = AddressStr.child("Si");
+        DatabaseReference gooRef = AddressStr.child("Goo");
+        DatabaseReference regTime = locationRef.child(key).child("Timestamp");
         //시간정보도 추가해야할듯
+
+        uid.setValue(FirebaseAuth.getInstance().getCurrentUser().getUid());
         siRef.setValue(Locality);
         gooRef.setValue(SubLocality);
+        regTime.setValue(ServerValue.TIMESTAMP);
+        //regTime.setValue()
         //일단 이렇게만
 
     }
